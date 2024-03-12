563cb58eab2883763696a05592375d5a0f51508f
jeypzz <jeypsruelo@gmail.com>
2024-03-12 13:03:37 +0800

    refactoring remove duplicate platform interfaces

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
 import apiClient, { FetchResponse } from "../services/api-client";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
