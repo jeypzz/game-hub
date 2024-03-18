01e99b5686de2a165a2cddb65d36b71858d13c47
jeypzz <jeypsruelo@gmail.com>
2024-03-18 14:09:06 +0800

    Setup Routing

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,34 @@
+import { Box, Grid, Show, GridItem, HStack } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+
+const Homepage = () => {
+  return (
+    <Grid
+      templateAreas={{ base: `"nav"`, lg: `"aside main"` }}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingLeft={"5px"}>
+          <GenreList />
+        </GridItem>
+      </Show>
+
+      <GridItem area="main">
+        <Box paddingLeft={7}>
+          <GameHeading />
+          <HStack spacing={5} marginBottom={5}>
+            <PlatformSelector />
+            <SortSelector />
+          </HStack>
+          <GameGrid />
+        </Box>
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default Homepage;
