11bea6c0c6fa51e3da3302a432e47db986c4a916
v88-Jp-Ruelo <jruelo@village88.com>
2024-03-18 10:43:25 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GameHeading from "./components/GameHeading";
 import GenreList from "./components/GenreList";
